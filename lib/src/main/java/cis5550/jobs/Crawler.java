6bf06c71b23b0181d3655fc64fdd547599df6a25
m0hdas <m.alsadhan@sms.ed.ac.uk>
2023-04-21 15:59:20 -0400

    crawler minor bug fixes

--- lib/src/main/java/cis5550/jobs/Crawler.java
+++ lib/src/main/java/cis5550/jobs/Crawler.java
@@ -7,6 +7,7 @@ import cis5550.kvs.Row;
 import cis5550.tools.Hasher;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.*;
@@ -56,181 +57,197 @@ public class Crawler {
         FlameRDD urlQueue;
         urlQueue = ctx.parallelize(normalised);
 
+        //add shutdown hook
+        FlameRDD finalUrlQueue = urlQueue;
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                System.out.println("Shutting down");
+                ctx.getKVS().persist("urlQueue");
+                finalUrlQueue.saveAsTable("urlQueue");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }));
+
         while (urlQueue.count() > 0) {
             urlQueue = urlQueue.flatMap(url -> {
-                // check we haven't already crawled this url
-                KVSClient kvs = ctx.getKVS();
-                if (kvs.existsRow("crawl", Hasher.hash(url))) {
-                    return List.of();
-                }
-
-                // check last time we accessed this host
-                URI uri = new URI(url);
-                String hostHash = Hasher.hash(uri.getHost());
-                if (hostHash == null) {
-                    return List.of();
-                }
-
-                // dont accept .jpg, .jpeg, .gif, .png, or .txt.
-                if (url.endsWith(".jpg") || url.endsWith(".jpeg") || url.endsWith(".gif")
-                        || url.endsWith(".png") || url.endsWith(".txt")) {
-                    return List.of();
-                }
-
-                // only accept urls with http or https
-                if (!url.startsWith("http://") && !url.startsWith("https://")) {
-                    return List.of();
-                }
-
-
-                if (kvs.existsRow("hosts", hostHash)) {
-                    Row hostRow = kvs.getRow("hosts", hostHash);
-                    String lastAccessString = hostRow.get("lastAccessed");
-                    if (lastAccessString == null) {
-                        hostRow.put("lastAccessed", String.valueOf(System.currentTimeMillis()));
-                    } else {
-                        long lastAccess = Long.parseLong(lastAccessString);
-                        if (System.currentTimeMillis() - lastAccess < 1000) {
-                            // we accessed recently, so don't crawl
-                            return List.of(url);
-                        }
-                    }
-                } else {
-                    // get robots.txt
-                    parseRobots(url, kvs);
-                }
-
-                // check if is in blacklist
-                for (Pattern pattern : blacklist) {
-                    Matcher matcher = pattern.matcher(url);
-                    if (matcher.matches()) {
+                try {
+                    // check we haven't already crawled this url
+                    KVSClient kvs = ctx.getKVS();
+                    if (kvs.existsRow("crawl", Hasher.hash(url))) {
                         return List.of();
                     }
-                }
 
-                // check if url is allowed by robots.txt
-                if (!isAllowed(kvs, url)) {
-                    return List.of();
-                }
-
-                System.out.println("Crawling " + url);
-
-                // establish connection + headers
-                HttpURLConnection connHEAD = (HttpURLConnection) new URL(url).openConnection();
-                connHEAD.setRequestProperty("User-Agent", "cis5550-crawler");
-                connHEAD.setConnectTimeout(1000);
-                connHEAD.setReadTimeout(1000);
-                connHEAD.setInstanceFollowRedirects(false);
-                // HEAD request
-                connHEAD.setRequestMethod("HEAD");
-                connHEAD.connect();
-
-                // handle redirects codes  (301, 302, 303, 307, 308)
-                int status = connHEAD.getResponseCode();
-                connHEAD.disconnect();
-                if (status == 301 || status == 302 || status == 303 || status == 307 || status == 308) {
-                    // save status code under current
-                    Row row = new Row(Hasher.hash(url));
-                    row.put("url", url);
-                    row.put("responseCode", String.valueOf(status));
-                    kvs.putRow("crawl", row);
-
-                    String redirectURL = connHEAD.getHeaderField("Location");
-                    String redirectNormalised = normaliseLink(url, redirectURL);
-                    assert redirectNormalised != null;
-                    return List.of(redirectNormalised);
-                }
-
-                if (connHEAD.getResponseCode() != 200) {
-                    Row row = new Row(Hasher.hash(url));
-                    row.put("url", url);
-                    row.put("page", connHEAD.getResponseMessage());
-                    row.put("responseCode", String.valueOf(status));
-                    kvs.putRow("crawl", row);
-                    return List.of();
-                }
-
-                // check content type
-                String contentType = connHEAD.getHeaderField("Content-Type");
-                if (contentType == null || !contentType.startsWith("text/html")) {
-                    Row row = new Row(Hasher.hash(url));
-                    row.put("url", url);
-                    row.put("page", connHEAD.getResponseMessage());
-                    row.put("responseCode", String.valueOf(status));
-                    if (contentType != null) {
-                        row.put("contentType", contentType);
+                    // check last time we accessed this host
+                    URI uri = new URI(url);
+                    String hostHash = Hasher.hash(uri.getHost());
+                    if (hostHash == null) {
+                        return List.of();
                     }
 
-                    String contentLength = connHEAD.getHeaderField("Content-Length");
+                    // dont accept .jpg, .jpeg, .gif, .png, or .txt.
+                    if (url.endsWith(".jpg") || url.endsWith(".jpeg") || url.endsWith(".gif")
+                            || url.endsWith(".png") || url.endsWith(".txt")) {
+                        return List.of();
+                    }
+
+                    // only accept urls with http or https
+                    if (!url.startsWith("http://") && !url.startsWith("https://")) {
+                        return List.of();
+                    }
+
+
+                    if (kvs.existsRow("hosts", hostHash)) {
+                        Row hostRow = kvs.getRow("hosts", hostHash);
+                        String lastAccessString = hostRow.get("lastAccessed");
+                        if (lastAccessString == null) {
+                            hostRow.put("lastAccessed", String.valueOf(System.currentTimeMillis()));
+                        } else {
+                            long lastAccess = Long.parseLong(lastAccessString);
+                            if (System.currentTimeMillis() - lastAccess < 500) {
+                                // we accessed recently, so don't crawl
+                                return List.of(url);
+                            }
+                        }
+                    } else {
+                        // get robots.txt
+                        parseRobots(url, kvs);
+                    }
+
+                    // check if is in blacklist
+                    for (Pattern pattern : blacklist) {
+                        Matcher matcher = pattern.matcher(url);
+                        if (matcher.matches()) {
+                            return List.of();
+                        }
+                    }
+
+                    // check if url is allowed by robots.txt
+                    if (!isAllowed(kvs, url)) {
+                        return List.of();
+                    }
+
+                    System.out.println("Crawling " + url);
+
+                    // establish connection + headers
+                    HttpURLConnection connHEAD = (HttpURLConnection) new URL(url).openConnection();
+                    connHEAD.setRequestProperty("User-Agent", "cis5550-crawler");
+                    connHEAD.setConnectTimeout(1000);
+                    connHEAD.setReadTimeout(1000);
+                    connHEAD.setInstanceFollowRedirects(false);
+                    // HEAD request
+                    connHEAD.setRequestMethod("HEAD");
+                    connHEAD.connect();
+
+                    // handle redirects codes  (301, 302, 303, 307, 308)
+                    int status = connHEAD.getResponseCode();
+                    connHEAD.disconnect();
+                    if (status == 301 || status == 302 || status == 303 || status == 307 || status == 308) {
+                        // save status code under current
+                        Row row = new Row(Hasher.hash(url));
+                        row.put("url", url);
+                        row.put("responseCode", String.valueOf(status));
+                        kvs.putRow("crawl", row);
+
+                        String redirectURL = connHEAD.getHeaderField("Location");
+                        String redirectNormalised = normaliseLink(url, redirectURL);
+                        assert redirectNormalised != null;
+                        return List.of(redirectNormalised);
+                    }
+
+                    if (connHEAD.getResponseCode() != 200) {
+                        Row row = new Row(Hasher.hash(url));
+                        row.put("url", url);
+                        row.put("page", connHEAD.getResponseMessage());
+                        row.put("responseCode", String.valueOf(status));
+                        kvs.putRow("crawl", row);
+                        return List.of();
+                    }
+
+                    // check content type
+                    String contentType = connHEAD.getHeaderField("Content-Type");
+                    if (contentType == null || !contentType.startsWith("text/html")) {
+                        Row row = new Row(Hasher.hash(url));
+                        row.put("url", url);
+                        row.put("page", connHEAD.getResponseMessage());
+                        row.put("responseCode", String.valueOf(status));
+                        if (contentType != null) {
+                            row.put("contentType", contentType);
+                        }
+
+                        String contentLength = connHEAD.getHeaderField("Content-Length");
+                        if (contentLength != null) {
+                            row.put("length", contentLength);
+                        }
+
+                        kvs.putRow("crawl", row);
+                        return List.of();
+                    }
+
+
+                    // GET request
+                    HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+                    conn.setRequestProperty("User-Agent", "cis5550-crawler");
+                    conn.setConnectTimeout(1000);
+                    conn.setReadTimeout(1000);
+                    conn.setRequestMethod("GET");
+                    conn.setInstanceFollowRedirects(false);
+                    conn.connect();
+
+                    if (conn.getResponseCode() != 200) {
+                        return List.of();
+                    }
+
+                    byte[] contentBytes = conn.getInputStream().readAllBytes();
+                    conn.disconnect();
+
+                    // content-seen test
+                    String pageHash = Hasher.hash(Arrays.toString(contentBytes));
+                    if (kvs.existsRow("seen", pageHash)) {
+                        Row row = new Row(Hasher.hash(url));
+                        row.put("url", url);
+                        String canonicalURL = kvs.getRow("seen", pageHash).get("url");
+                        row.put("canonicalURL", canonicalURL);
+                        row.put("responseCode", "200");
+                        row.put("contentType", "text/html");
+                        System.out.println("Content seen: " + url + " is the same as " + canonicalURL);
+                        kvs.putRow("crawl", row);
+                        return List.of();
+                    }
+
+                    // add to "crawl" table
+                    Row row = new Row(Hasher.hash(url));
+                    row.put("url", url);
+                    row.put("page", contentBytes);
+                    row.put("responseCode", "200");
+                    row.put("contentType", "text/html");
+
+                    String contentLength = conn.getHeaderField("Content-Length");
+                    System.out.println("Content length: " + contentLength);
                     if (contentLength != null) {
                         row.put("length", contentLength);
                     }
-
                     kvs.putRow("crawl", row);
+
+                    // add to seen table
+                    Row seenRow = new Row(pageHash);
+                    seenRow.put("url", url);
+                    kvs.putRow("seen", seenRow);
+
+                    // extract links
+                    List<String> links = extractLinks(new String(contentBytes), url);
+                    System.out.println("Found " + links.size() + " links in " + url + ":");
+                    for (String link : links) {
+                        System.out.println(link);
+                    }
+                    return links;
+                } catch (Exception e) {
+                    System.out.println("Error crawling " + url);
+                    e.printStackTrace();
                     return List.of();
                 }
-
-
-                // GET request
-                HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
-                conn.setRequestProperty("User-Agent", "cis5550-crawler");
-                conn.setConnectTimeout(1000);
-                conn.setReadTimeout(1000);
-                conn.setRequestMethod("GET");
-                conn.setInstanceFollowRedirects(false);
-                conn.connect();
-
-                if (conn.getResponseCode() != 200) {
-                    return List.of();
-                }
-
-                byte[] contentBytes = conn.getInputStream().readAllBytes();
-                conn.disconnect();
-
-                // content-seen test
-                String pageHash = Hasher.hash(Arrays.toString(contentBytes));
-                if (kvs.existsRow("seen", pageHash)) {
-                    Row row = new Row(Hasher.hash(url));
-                    row.put("url", url);
-                    String canonicalURL = kvs.getRow("seen", pageHash).get("url");
-                    row.put("canonicalURL", canonicalURL);
-                    row.put("responseCode", "200");
-                    row.put("contentType", "text/html");
-                    System.out.println("Content seen: " + url + " is the same as " + canonicalURL);
-                    kvs.putRow("crawl", row);
-                    return List.of();
-                }
-
-                // add to "crawl" table
-                Row row = new Row(Hasher.hash(url));
-                row.put("url", url);
-                row.put("page", contentBytes);
-                row.put("responseCode", "200");
-                row.put("contentType", "text/html");
-
-                String contentLength = conn.getHeaderField("Content-Length");
-                System.out.println("Content length: " + contentLength);
-                if (contentLength != null) {
-                    row.put("length", contentLength);
-                }
-                kvs.putRow("crawl", row);
-
-                // add to seen table
-                Row seenRow = new Row(pageHash);
-                seenRow.put("url", url);
-                kvs.putRow("seen", seenRow);
-
-                // extract links
-                List<String> links = extractLinks(new String(contentBytes), url);
-                System.out.println("Found " + links.size() + " links in " + url + ":");
-                for (String link : links) {
-                    System.out.println(link);
-                }
-                return links;
             });
 
-            // rate limit
-            Thread.sleep(100);
         }
     }
 
@@ -257,7 +274,7 @@ public class Crawler {
 
 
             // If resulting URL is empty, discard it
-            if (linkURI.toString().isEmpty()) {
+            if (linkURI.toString().isBlank()) {
                 return null;
             }
 
@@ -283,6 +300,7 @@ public class Crawler {
             }
 
             String path = linkURI.getPath().isBlank() ? "/" : linkURI.getPath();
+            path = path.replaceAll("&#..;", "/");
 
             //remove fragment
             linkURI = new URI(linkURI.getScheme(), linkURI.getUserInfo(), linkURI.getHost(), linkURI.getPort(),
@@ -356,11 +374,6 @@ public class Crawler {
         }
 
         return true;
-    }
-
-    public static void main(String[] args) throws IOException {
-        URL url = new URL("http://localhost:8001/ping?id=gwefa&port=9001");
-        url.getContent();
     }
 
 }
